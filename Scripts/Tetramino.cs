e883a32072f41f9416dd33f91e77349d062ddb9d
0101kvmt <0101kvmt@gmail.com>
2018-01-08 01:27:33 -0800

    Input set-up

--- Scripts/Tetramino.cs
+++ Scripts/Tetramino.cs
@@ -11,6 +11,23 @@ public class Tetramino : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        UserInput();
 	}
+
+    void UserInput ()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        } else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        } else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.Rotate (0, 0, -90);
+        } else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Rotate (0, 0, 90);
+        }
+    }
 }
