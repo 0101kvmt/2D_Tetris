4792262f432d2841ef3910beeb35fd70d5511fd1
0101kvmt <0101kvmt@gmail.com>
2018-01-08 20:54:51 -0800

    set grid limits

--- Scripts/Tetramino.cs
+++ Scripts/Tetramino.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Tetramino : MonoBehaviour {
 
+    float fall = 0;
+    public float fallSpeed = 1;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -19,15 +22,66 @@ public class Tetramino : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             transform.position += new Vector3(1, 0, 0);
+
+            if (CheckIsValidPosition())
+            {
+                
+            } else
+            {
+                transform.position += new Vector3(-1, 0, 0);
+            }
         } else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             transform.position += new Vector3(-1, 0, 0);
-        } else if (Input.GetKeyDown(KeyCode.DownArrow))
+
+            if (CheckIsValidPosition())
+            {
+
+            }
+            else
+            {
+                transform.position += new Vector3(1, 0, 0);
+            }
+        } else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fallSpeed)
         {
-            transform.Rotate (0, 0, -90);
+            transform.position += new Vector3(0, -1, 0);
+            if (CheckIsValidPosition())
+            {
+
+            }
+            else
+            {
+                transform.position += new Vector3(0, 1, 0);
+            }
+
+            fall = Time.time;
         } else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             transform.Rotate (0, 0, 90);
+            if (CheckIsValidPosition())
+            {
+
+            } else
+            {
+                transform.Rotate(0, 0, -90);
+            }
         }
+    }
+
+    bool CheckIsValidPosition ()
+    {
+        foreach (Transform mino in transform)
+        {
+            // rounded value of current game object
+            Vector2 pos = FindObjectOfType<Game>().Round(mino.position);
+
+            // if not inside grid, return false
+            if(FindObjectOfType<Game>().CheckIsInsideGrid (pos) == false)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
